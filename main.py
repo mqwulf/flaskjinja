ce6a7e08c9ccfadc3870c260b75f58b18def114b
Buyantogtokh Khurelbaatar <vicekurel@gmail.com>
2024-11-12 13:26:36 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    random_number = random.randint(1, 10)
+    return render_template("index.html", rand = random_number)
+
+if __name__ == "__main__":
+    app.run(debug=True)
